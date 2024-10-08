51df680e93cfa06b154bf536c3dec26b478b1b7b
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2024-10-08 02:43:03 -0600

    video 59

--- plataformas/new-game-project/Trampas/sierra.gd
+++ plataformas/new-game-project/Trampas/sierra.gd
@@ -3,11 +3,36 @@ extends Node2D
 
 var dmg = 1
 
-@onready var sprite = $SierraVerdadera/Sprite
+@onready var sprite = $SierraVerdadera/Sprite2D
 @onready var path = $Path2D/PathFollow2D
+@onready var sierra =$SierraVerdadera
+@export var platformSpeed : float = .2
+var isRigth = true
+
+
 
 func _process(delta):
 	sprite.rotate(deg_to_rad(450 * delta))
+	sierra.global_position = path.global_position
+	if isRigth:
+		path.progress_ratio += platformSpeed * delta
+	
+	else :
+		path.progress_ratio -= platformSpeed * delta
+	
+	if path.progress_ratio >= .95:
+		isRigth = false
+	
+	elif path.progress_ratio <= .05:
+		isRigth = true
+	
+	
+	
+	
+	
+	
+
+
 
 func _on_dmg_player_body_entered(body):
 	if body is Player:
